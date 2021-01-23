164e4e2e87a1521bcd0b3d7aa21639eacf329a9e
JonaOlave <jonathan.olave@outlook.com>
2021-01-23 01:05:58 -0300

    login ingresado

--- ejercicio02/usuarios/urls.py
+++ ejercicio02/usuarios/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import SignUpView
+from .views import Registro
 
 app_name = "usuarios"
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.Principal.as_view(), name = 'principal'),
     path('login/', auth_views.LoginView.as_view(template_name = 'usuarios/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'usuarios/login.html'), name = 'logout'),
-    path('registo/', SignUpView.as_view(), name='registro')
+    path('registo/', Registro.as_view(), name='registro')
 
 ]
--- ejercicio02/usuarios/views.py
+++ ejercicio02/usuarios/views.py
@@ -11,7 +11,7 @@ class Principal(LoginRequiredMixin, generic.TemplateView):
     template_name = 'usuarios/principal.html'
     login_url='usuarios:login'
 
-class SignUpView(generic.CreateView):
+class Registro(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('principal')
+    success_url = reverse_lazy('usuarios:principal')
     template_name = 'usuarios/registro.html'
